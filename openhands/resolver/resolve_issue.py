8997af244b1e977a809d10be15ce76d322749531
Fazek007 <144393947+Fazek007@users.noreply.github.com>
2025-03-27 21:34:09 +0100

    Update resolve_issue.py

--- openhands/resolver/resolve_issue.py
+++ openhands/resolver/resolve_issue.py
@@ -233,9 +233,6 @@ async def process_issue(
             sandbox_config.user_id = get_unique_uid()
 
     logger.info(f"workspace_base: {workspace_base}")
-    logger.info(f"workspace_mount_path: {workspace_mount_path}")
-    logger.info(f"workspace_mount_path_in_sandbox: {workspace_mount_path_in_sandbox}")
-    logger.info(f"workspace_mount_rewrite: {workspace_mount_rewrite}")
     
     config = AppConfig(
         default_agent='CodeActAgent',
@@ -248,16 +245,13 @@ async def process_issue(
         # do not mount workspace
         workspace_base=workspace_base,
         workspace_mount_path=workspace_base,
-        workspace_mount_path_in_sandbox="test/workspace",
+        workspace_mount_path_in_sandbox="/workspace",
         #workspace_mount_rewrite = f"/home/jovyan/work/nas:/mnt/nas",
         workspace_mount_rewrite = f"{workspace_base}:/home/jovyan/work/nas/USERS/rigobence/EXPERIMENTS/openhands/output/workspace/issue_64",
         agents={'CodeActAgent': AgentConfig(disabled_microagents=['github'])},
     )
 
     logger.info(f"workspace_base: {workspace_base}")
-    logger.info(f"workspace_mount_path: {workspace_mount_path}")
-    logger.info(f"workspace_mount_path_in_sandbox: {workspace_mount_path_in_sandbox}")
-    logger.info(f"workspace_mount_rewrite: {workspace_mount_rewrite}")
     
     config.set_llm_config(llm_config)
 
